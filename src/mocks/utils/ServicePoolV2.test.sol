0bc782fd7971390db66bbfd76b6f57ae3b767970
Nikos Fotiadis <nikosfotiadis0@gmail.com>
2023-06-26 17:35:20 +0300

    test: added test for service pool

--- src/mocks/utils/ServicePoolV2.test.sol
+++ src/mocks/utils/ServicePoolV2.test.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import { BurnPool } from "../../BurnPool.sol";
+import { ServicePool } from "../../ServicePool.sol";
 
 /**
  * @title RewardPool contract upgrade.
@@ -9,7 +9,7 @@ import { BurnPool } from "../../BurnPool.sol";
  * @notice This constract serves as the testing suite for upgradeability.
  *
  * */
-contract BurnPoolV2 is BurnPool {
+contract ServicePoolV2 is ServicePool {
   // add this to be excluded from coverage report
   function test() public {}
 
